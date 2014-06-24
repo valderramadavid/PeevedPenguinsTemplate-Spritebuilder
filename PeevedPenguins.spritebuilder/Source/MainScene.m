98f239ed96369573c491c694a5eed74800bce5af
epimetheusincarnate <dvalderrama08@gmail.com>
2014-06-24 10:23:11 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -12,7 +12,8 @@
 
 -(void)play
 {
-    CCLOG(@"play button pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
